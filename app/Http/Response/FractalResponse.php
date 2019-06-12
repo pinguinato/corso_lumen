a95376a227239e09c54e44b2a5b35ab0ba446a89
Roberto Gianotto <rgianotto-ext@skylogic.it>
2019-06-12 14:41:17 +0000

    Aggiunta e completamento della FractalResponse

--- app/Http/Response/FractalResponse.php
+++ app/Http/Response/FractalResponse.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Response;
 
+use League\Fractal\Manager;
+use League\Fractal\Resource\ResourceInterface;
+use League\Fractal\Serializer\SerializerAbstract;
+use League\Fractal\TransformerAbstract;
+use League\Fractal\Resource\Item;
+use League\Fractal\Resource\Collection;
+
+
 class FractalResponse
 {
-    public function __construct()
+    /**
+     * @var Manager
+     */
+    private $manager;
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+
+    public function __construct(Manager $manager, SerializerAbstract $serializer)
     {
+        $this->manager = $manager;
+        $this->serializer = $serializer;
+        $this->manager->setSerializer($serializer);
+    }
+
+    /**
+     * @param $data
+     * @param TransformerAbstract $transformer
+     * @param null $resourceKey
+     * @return array
+     */
+    public function item($data, TransformerAbstract $transformer, $resourceKey = null)
+    {
+//        $resource = new Item($data, $transformer, $resourceKey);
+
+//        return $this->manager->createData($resource)->toArray();
+
+        return $this->createDataArray(
+          new Item($data, $transformer, $resourceKey)
+        );
+    }
+
+    /**
+     * @param $data
+     * @param TransformerAbstract $transformer
+     * @param null $resourceKey
+     * @return array
+     */
+    public function collection($data, TransformerAbstract $transformer, $resourceKey = null)
+    {
+//        $resource = new Collection($data, $transformer, $resourceKey);
+
+//        return $this->manager->createData($resource)->toArray();
+
+        return $this->createDataArray(
+            new Collection($data, $transformer, $resourceKey)
+        );
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     * @return array
+     */
+    public function createDataArray(ResourceInterface $resource)
+    {
+        return $this->manager->createData($resource)->toArray();
     }
 }
--- tests/app/Http/Response/FractalResponseTest.php
+++ tests/app/Http/Response/FractalResponseTest.php
@@ -1,12 +1,81 @@
 <?php
 
 use App\Http\Response\FractalResponse;
+use Mockery as m;
+use \League\Fractal\Manager;
+use League\Fractal\Serializer\SerializerAbstract;
 
 class FractalResponseTest extends TestCase
 {
     /** @test */
-    public function it_can_be_initialized()
+    public function testItCanBeInitialized()
     {
-        $this->assertInstanceOf(FractalResponse::class, new FractalResponse());
+        $this->assertInstanceOf(FractalResponse::class, new FractalResponse(new Manager(), new League\Fractal\Serializer\ArraySerializer()));
+    }
+
+    /** @test */
+    public function testItCanBeInitializedRefactor()
+    {
+        // dipendenze di Fractal
+        $fractal_manager = m::mock(Manager::class);
+        $fractal_serializer = m::mock(SerializerAbstract::class);
+
+        $fractal_manager->shouldReceive('setSerializer')->with($fractal_serializer)->once()->andReturn($fractal_manager);
+
+        $fractal = new FractalResponse($fractal_manager, $fractal_serializer);
+
+        $this->assertInstanceOf(FractalResponse::class, $fractal);
+    }
+
+    /** @test */
+    public function testItCanTransformAnItem()
+    {
+        //transformer
+        $transformer = m::mock('League\Fractal\TransformerAbstract');
+
+        // scope
+        $scope = m::mock('League\Fractal\Scope');
+        $scope->shouldReceive('toArray')->once()->andReturn(['foo' => 'bar']);
+
+        // Serializer
+        $serializer = m::mock('League\Fractal\Serializer\SerializerAbstract');
+        $manager = m::mock('League\Fractal\Manager');
+        $manager->shouldReceive('setSerializer')->with($serializer)->once();
+        $manager->shouldReceive('createData')->once()->andReturn($scope);
+        $subject = new FractalResponse($manager, $serializer);
+
+        $this->assertInternalType('array', $subject->item(['foo' => 'bar'], $transformer));
+    }
+
+    public function testItCanTransformACollection()
+    {
+        $data = [
+            [
+                'foo' => 'bar'
+            ],
+            [
+                'fizz' => 'buzz'
+            ]
+        ];
+
+        //transformer
+        $transformer = m::mock('League\Fractal\TransformerAbstract');
+
+        // scope
+        $scope = m::mock('League\Fractal\Scope');
+        $scope->shouldReceive('toArray')->once()->andReturn($data);
+
+        // Serializer
+        $serializer = m::mock('League\Fractal\Serializer\SerializerAbstract');
+
+        $manager = m::mock('League\Fractal\Manager');
+
+        $manager->shouldReceive('setSerializer')->with($serializer)->once();
+
+        $manager->shouldReceive('createData')->once()->andReturn($scope);
+
+        $subject = new FractalResponse($manager, $serializer);
+
+        $this->assertInternalType('array', $subject->collection($data, $transformer));
     }
 }
